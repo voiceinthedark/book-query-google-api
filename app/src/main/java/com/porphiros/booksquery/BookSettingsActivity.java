c57722e0be47a590da9c916befa0f68341a55e47
porphiros <darkrisingforce@gmail.com>
2018-07-25 07:04:55 +0300

    Add Settings activity

---
+++ app/src/main/java/com/porphiros/booksquery/BookSettingsActivity.java
@@ -0,0 +1,52 @@
+package com.porphiros.booksquery;
+
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_settings);
+    }
+
+    public static class BooksSettingsFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener{
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            //Add preference layout from our xml folder
+            addPreferencesFromResource(R.xml.settings_activity);
+
+            //catch the limit Preference
+            Preference limitPreference = findPreference(getString(R.string.settings_limit_key));
+            bindPreferenceValueToSummary(limitPreference);
+
+            //catch the order Preference
+            Preference orderPreference = findPreference(getString(R.string.settings_order_key));
+            bindPreferenceValueToSummary(orderPreference);
+        }
+
+        public void bindPreferenceValueToSummary(Preference preference){
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences sharedPreferences =
+                    PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = sharedPreferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            return false;
+        }
+    }
+}
